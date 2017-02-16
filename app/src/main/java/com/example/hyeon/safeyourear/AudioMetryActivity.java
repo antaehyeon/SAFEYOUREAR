e6b99cf24314120666b7a32d69d80e62eb4a2657
안태현 <antaehyeon@live.co.kr>
2017-02-17 02:17:49 +0900

    Graph (Chart) 한종류 추가 훨씬 가독성이 좋고 기능이 많다 코드 분석만 해서 원하는 입맛대로 바꾸면 될듯 함 https://github.com/lecho/hellocharts-android

--- app/src/main/java/com/example/hyeon/safeyourear/AudioMetryActivity.java
+++ app/src/main/java/com/example/hyeon/safeyourear/AudioMetryActivity.java
@@ -14,8 +14,17 @@ import android.widget.TextView;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import im.dacer.androidcharts.LineView;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class AudioMetryActivity extends AppCompatActivity {
 
@@ -25,6 +34,29 @@ public class AudioMetryActivity extends AppCompatActivity {
 
     int whiteColor = Color.parseColor("#FFFFFF");
     int baseColor = Color.parseColor("#213b4c");
+
+    LineChartView chart;
+    private LineChartData data;
+
+
+    // 그래프 V2 변수들
+    int numberOfLines = 1;
+    int maxNumberOfLines = 4;
+    int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasLines = true;
+    private boolean hasPoints = true;
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = false;
+    private boolean hasLabels = false;
+    private boolean isCubic = false;
+    private boolean hasLabelForSelected = false;
+    private boolean pointsHaveDifferentColor;
+    private boolean hasGradientToTransparent = false;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -36,25 +68,26 @@ public class AudioMetryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_metry);
 
-//        getSupportActionBar().setTitle("청력측정");
-//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF07263b));
-
+        // ActionBar 대신 ToolBar 생성
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar_audiometry);
         toolbar.setTitleTextColor(whiteColor);
         toolbar.setBackgroundColor(baseColor);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
 
+        // Toolbar 에 넣을 txetView(title) 속성 조정
         title = (TextView) toolbar.findViewById(R.id.toolBar_audiometry_title);
         title.setTextColor(whiteColor);
         title.setTypeface(title.getTypeface(), Typeface.BOLD);
         title.setTextSize(20);
 
+        // audiometry 최상위 뷰 속성 조정
         relativeLayout = (RelativeLayout) findViewById(R.id.activity_audio_metry);
         relativeLayout.setBackgroundColor(baseColor);
         relativeLayout.setPadding(0, 0, 0, 0);
 
 
+        // Notification Bar 색상 지정
         //https://medium.com/@mindwing/actionbar-%EB%A5%BC-%EB%8B%A4%EB%A4%84%EB%B4%85%EC%8B%9C%EB%8B%A4-401709e5480d#.jsyw89cbu
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -64,7 +97,7 @@ public class AudioMetryActivity extends AppCompatActivity {
         window.setStatusBarColor(baseColor);
 
 
-
+        // 그래프 그리기
         LineView lineView = (LineView) relativeLayout.findViewById(R.id.line_view);
         initView(lineView);
 //        lineView.setDrawDotLine(false); //optional
@@ -83,9 +116,25 @@ public class AudioMetryActivity extends AppCompatActivity {
 
         lineView.setDataList(dataLists); //or lineView.setFloatDataList(floatDataLists)
 
+        // 그래프 그리기 - HelloChart
+        chart = (LineChartView) relativeLayout.findViewById(R.id.chart);
+
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) Math.random() * 100f;
+            }
+        }
+
+        generateData();
+
+        // Disable viewport recalculations, see toggleCubic() method for more info.
+        chart.setViewportCalculationEnabled(false);
+
+        resetViewport();
 
     }
 
+    // 그래프 초기화 메소드
     private void initView(LineView lineView) {
         ArrayList<String> test = new ArrayList<String>();
         for (int i=0; i<9; i++){
@@ -97,4 +146,64 @@ public class AudioMetryActivity extends AppCompatActivity {
         lineView.setShowPopup(LineView.SHOW_POPUPS_NONE);
 
     }
+
+    // 그래프 V2
+    private void generateData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i]);
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(hasLabels);
+            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            //line.setHasGradientToTransparent(hasGradientToTransparent);
+            if (pointsHaveDifferentColor){
+                line.setPointColor(ChartUtils.COLORS[(i + 1) % ChartUtils.COLORS.length]);
+            }
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis().setHasLines(true);
+            if (hasAxesNames) {
+                axisX.setName("Axis X");
+                axisY.setName("Axis Y");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        chart.setLineChartData(data);
+
+    }
+
+    private void resetViewport() {
+        // Reset viewport height range to (0,100)
+        final Viewport v = new Viewport(chart.getMaximumViewport());
+        v.bottom = 0;
+        v.top = 100;
+        v.left = 0;
+        v.right = numberOfPoints - 1;
+        chart.setMaximumViewport(v);
+        chart.setCurrentViewport(v);
+    }
+
 }
